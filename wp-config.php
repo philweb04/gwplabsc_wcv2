765d70e1ce166cf47a6eb7dd24a69d7eb434dc1c
philwebservices.programmer04@gmail.com <philwebservices.programmer04@gmail.com>
2015-09-02 13:41:40 +0800

    config

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'kulayful_db');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'gwplabsc_wcv2');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'I7hH+R0hU!RG');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
