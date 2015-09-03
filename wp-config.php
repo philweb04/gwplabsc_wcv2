f84de84802559ee075381f1df47b2555c6f55d58
philwebservices.programmer04@gmail.com <philwebservices.programmer04@gmail.com>
2015-09-03 13:53:25 +0800

    config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'kulayful_db');
+define('DB_NAME', 'gwplabsc_wcv2');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'gwplabsc_uwcv2');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'I7hH+R0hU!RG');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
